609ef395fd606fc57c834ccba6da6a391746111a
sagarshetty-21 <5agarm5hetty05@gmail.com>
2024-07-02 16:46:20 +0530

    changes

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+const crypto = require('crypto');
+
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
+    },
+  },
+});
